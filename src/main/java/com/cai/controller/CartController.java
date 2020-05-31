5b0b5b8bbd9bdc38daf80bcfbe6b69387f3b0af1
lln <943755151@qq.com>
2020-05-31 17:42:06 +0800

    1

--- src/main/java/com/cai/controller/CartController.java
+++ src/main/java/com/cai/controller/CartController.java
@@ -1,8 +1,21 @@
 package com.cai.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cai.model.Cart;
+import com.cai.utils.PostUtils;
+import okhttp3.FormBody;
+
+import java.util.List;
+
 public class CartController {
-    public static void test(){
-        System.out.println("Hello world alt+insert");
+    public static List<Cart> list(int uid){
+        String url = "/cart/list";
+        FormBody.Builder param = new FormBody.Builder();
+        param.add("uid", "" + uid);
+        JSONObject body = PostUtils.postParam(url,param);
+        JSONArray list = body.getJSONArray("data");
+        return list.toJavaList(Cart.class);
     }
-    
+
 }
--- test/com/cai/controller/CartControllerTest.java
+++ test/com/cai/controller/CartControllerTest.java
@@ -1,13 +1,33 @@
 package com.cai.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cai.model.Cart;
+import com.cai.model.Product;
+import com.cai.utils.PostUtils;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okio.BufferedSink;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class CartControllerTest {
 
     @Test
-    public void test1() {
-        CartController.test();
+    public void listCart() {
+        int uid = 1;
+        System.out.printf("uid为"+uid+"的用户的购物车");
+        List<Cart> cart1 = CartController.list(uid);
+        for(Cart cart:cart1){
+            System.out.println(cart);
+        }
     }
 }
